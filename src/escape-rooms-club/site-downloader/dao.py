6f4d664e6b83d7e7cc4a168e67e98eeddaa634bf
Pavel Krayzel <pavel.krayzel@made.com>
2019-02-12 22:18:48 +0100

    Integration test finished

--- src/escape-rooms-club/site-downloader/dao.py
+++ src/escape-rooms-club/site-downloader/dao.py
@@ -1,5 +1,5 @@
 import boto3
-from botocore.vendored import requests
+import requests
 import logging
 
 logger = logging.getLogger()
@@ -16,7 +16,8 @@ class WebClient:
 
 class StorageClient:
 
-    client = boto3.client('s3')
+    def __init__(self, endpoint_url=None):
+        self.client = boto3.client('s3', endpoint_url=endpoint_url)
 
     def upload(self, bucket_name, file_key, file_content):
         logger.info(f'Uploading file content to the bucket: {bucket_name} to the path: {file_key}')
@@ -26,3 +27,22 @@ class StorageClient:
             Key=file_key
         )
 
+    def find_all_file_keys_in_directory(self, bucket_name, directory):
+        logger.info(f'Listing all file keys in directory {directory} in the bucket: {bucket_name}')
+        response = self.client.list_objects_v2(
+            Bucket=bucket_name,
+            Prefix=f'{directory}/' 
+        )
+        if response['KeyCount'] == 0:
+            return []
+
+        return [item['Key'] for item in response['Contents']]
+
+
+    def get(self, bucket_name, file_key):
+        logger.info(f'Downloading file content for the file: {file_key} from the bucket: {bucket_name}')
+        response = self.client.get_object(
+            Bucket=bucket_name,
+            Key=file_key
+        )
+        return response['Body'].read()
---
+++ src/escape-rooms-club/site-downloader/tests/integration/test_site_downloader.py
@@ -0,0 +1,69 @@
+import os
+import sys
+sys.path.insert(1, os.path.join(sys.path[0], '../..'))
+
+from domain import SiteDownloader
+from dao import WebClient, StorageClient
+
+
+def test_site_downloader_success():
+    storage_client = StorageClient(endpoint_url="http://localhost:4572")
+    site_downloader = SiteDownloader(WebClient(), storage_client)
+
+    bucket_name = "scrapers-output"
+    target_directory = "exitgames.cz"
+    success, error_message = site_downloader.download_site_and_store_its_content(
+        site_url="http://localhost:5000/exitgames.cz", 
+        bucket_name=bucket_name, 
+        target_directory=target_directory
+    )
+
+    assert success is True, "Operation should have succeeded"
+    assert not error_message, "Error message should be none"
+
+    file_keys = storage_client.find_all_file_keys_in_directory(
+        bucket_name=bucket_name, 
+        directory=target_directory
+    )
+    # we expect the last found file to be the one created during this test
+    last_file_content = storage_client.get(
+        bucket_name=bucket_name, 
+        file_key=file_keys[-1])
+    
+    assert 'href="https://www.exitgames.cz/seznam-unikovych-her"' in last_file_content.decode('UTF-8')
+
+
+def test_site_downloader_web_client_failure():
+    storage_client = StorageClient(endpoint_url="http://localhost:4572")
+    site_downloader = SiteDownloader(WebClient(), storage_client)
+
+    site_url = "http://localhost:5000/not-existing-site.cz"
+    bucket_name = "scrapers-output"
+    target_directory = "not-existing-site.cz"
+
+    success, error_message = site_downloader.download_site_and_store_its_content(
+        site_url=site_url, 
+        bucket_name=bucket_name, 
+        target_directory=target_directory
+    )
+
+    assert success is False, "Operation should have failed"
+    assert site_url in error_message, "Error message should not be none and should contain site_url"
+
+
+def test_site_downloader_storage_client_failure():
+    storage_client = StorageClient(endpoint_url="http://localhost:4572")
+    site_downloader = SiteDownloader(WebClient(), storage_client)
+
+    site_url = "http://localhost:5000/exitgames.cz"
+    bucket_name = "not-existing-bucket"
+    target_directory = "exitgames.cz"
+
+    success, error_message = site_downloader.download_site_and_store_its_content(
+        site_url=site_url, 
+        bucket_name=bucket_name, 
+        target_directory=target_directory
+    )
+
+    assert success is False, "Operation should have failed"
+    assert 'The specified bucket does not exist' in error_message, "Error message should not be none"
